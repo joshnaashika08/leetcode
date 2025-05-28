538e23f7612de50e6ca147e6675a6ab6a2fa1c5d
Joshna Ashika.M <aashikam2005@gmail.com>
2025-05-28 21:35:52 +0530

    Time: 100 ms (57.55%), Space: 145.6 MB (80.34%) - LeetHub

---
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& r) {
+        long long count=0;
+        unordered_map<double,int>m;
+        int n=r.size();
+        for(int i=0;i<n;i++){
+            double ratio=(double)r[i][0]/r[i][1];
+            m[ratio]=m[ratio]+1;
+        }
+        for(auto i:m){
+            long long temp=i.second;
+            count+=temp*(temp-1)/2;
+        }
+        return count;
+        
+    }
+};
