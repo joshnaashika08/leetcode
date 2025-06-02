18858b4a32c74ea3a167fc92ec2e5801363d55c3
Joshna Ashika.M <aashikam2005@gmail.com>
2025-06-02 11:14:42 +0530

    Time: 0 ms (100%), Space: 7.6 MB (86.92%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.c
@@ -0,0 +1,12 @@
+bool isThree(int n) {
+    int count=0;
+    for(int i=1;i*i<=n;i++){
+        if(n%i==0){
+            count++;
+            if(i!=n/i){
+                count++;
+            }
+        }
+    }
+    return count==3;
+}
