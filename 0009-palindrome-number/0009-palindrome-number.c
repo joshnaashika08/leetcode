ec024fbd13e5855ee577830f4a020fe1841c2a2a
Joshna Ashika.M <aashikam2005@gmail.com>
2025-06-02 11:27:43 +0530

    Time: 0 ms (100%), Space: 8.1 MB (82.26%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,12 @@
+bool isPalindrome(int x) {
+    if(x<0) return false;
+    int org=x;
+    long reversed =0;
+    while(x>0)
+    {
+        reversed=reversed*10+(x%10);
+        x/=10;
+
+    }
+    return org== reversed;
+}
